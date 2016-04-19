96e279164a2957abe90a9d94ad731140f2f302d0
oromand <oliv.rm@gmail.com>
2016-04-19 20:04:14 +0200

    Renamed projects and solution

--- Ixcys.Tennis.Console/Properties/AssemblyInfo.cs
+++ Ixcys.Tennis.Console/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ixcys.Tennis.Console")]
+[assembly: AssemblyTitle("Sport.Scoring.Tennis.Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ixcys.Tennis.Console")]
+[assembly: AssemblyProduct("Sport.Scoring.Tennis.Console")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Ixcys.Tennis/Properties/AssemblyInfo.cs
+++ Ixcys.Tennis/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ixcys.Tennis")]
+[assembly: AssemblyTitle("Sport.Scoring.Tennis")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ixcys.Tennis")]
+[assembly: AssemblyProduct("Sport.Scoring.Tennis")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
