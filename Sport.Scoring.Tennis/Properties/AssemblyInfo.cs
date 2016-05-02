542ac60a558c143bff89cabf7fe37e94ed9ea5b3
oromand <oliv.rm@gmail.com>
2016-05-02 21:27:34 +0200

    Renamed assembly

--- Sport.Scoring.Tennis/Properties/AssemblyInfo.cs
+++ Sport.Scoring.Tennis/Properties/AssemblyInfo.cs
@@ -34,3 +34,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("Sport.Scoring.Tennis.Tests")]
+[assembly: InternalsVisibleTo("Ixcys.Tennis.Explorables")]
+[assembly: InternalsVisibleTo("Sport.Scoring.Tennis.Tests1")]
+
